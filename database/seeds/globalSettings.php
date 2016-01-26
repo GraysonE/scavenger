8cb20794be1826e2becea155e437a1ca14bb0e1f
Grayson Erhard <grow@graysonerhard.com>
2016-01-25 19:29:26 -0700

    fixed seeder

--- database/seeds/globalSettings.php
+++ database/seeds/globalSettings.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Scavenger\SocialMediaAccount;
 
 class globalSettings extends Seeder
 {
@@ -11,18 +12,13 @@ class globalSettings extends Seeder
      */
     public function run()
     {
-        $social_media_account_values = [
-	        
-		    ['consumer_key' => '4nJh80KE5bVyuyY9WlT5rTYlN'], 
-		    ['consumer_secret' => 'dYvw5tYAc2xnoKw3wdHc13oIdq6i7kEt5ndaxfoA4d0kUgNPmw'], 
-		    ['access_token' => '1286744324-DwTdCgdd8VrwOvEqJY6jWZjkomtijdmR4vbEvQF'],
-		    ['access_token_secret' => 'FYSZyvgQlT0V2JasCzlAoOPIaVRVpTKANEn4PaTbqd7uz']
-		];
-	    
-	    foreach ($social_media_account_values as $social_media_account_value)
-	    {
-//		    $db =
-	    }
-		DB::table('social_media_accounts')->insert($social_media_account_value);
+
+		$socialMediaAccount = new SocialMediaAccount();
+
+		$socialMediaAccount->consumer_key = '4nJh80KE5bVyuyY9WlT5rTYlN';
+		$socialMediaAccount->consumer_secret = 'dYvw5tYAc2xnoKw3wdHc13oIdq6i7kEt5ndaxfoA4d0kUgNPmw';
+		$socialMediaAccount->access_token = '1286744324-DwTdCgdd8VrwOvEqJY6jWZjkomtijdmR4vbEvQF';
+		$socialMediaAccount->access_token_secret = 'FYSZyvgQlT0V2JasCzlAoOPIaVRVpTKANEn4PaTbqd7uz';
+		$socialMediaAccount->save();
     }
 }
